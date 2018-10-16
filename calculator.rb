30ae753bb5e3b3d7b876481e5b79e4189d896bba
kevintudiep <kevintudiep@gmail.com>
2018-10-16 11:31:45 -0500

    completed exercise

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+class Calculator
+
+  def adds_0_and_0(first_number, second_number)
+    return first_number + second_number
+  end
+
+  def subtract(first_number, second_number)
+    return first_number - second_number
+  end
+
+  def sum(array)
+    return array.sum
+  end
+
+end
